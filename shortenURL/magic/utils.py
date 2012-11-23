639cc7ad3980e2fed68a09d64424818ea0b2c40f
Wenbin Wu <wenbin87@gmail.com>
2012-11-23 10:09:26 +0000

    rm comment

--- shortenURL/magic/utils.py
+++ shortenURL/magic/utils.py
@@ -5,9 +5,7 @@ import hashlib, random, string
 # 1. because we query database every time 
 #    we want to decode a shorten URL
 #    so there is no need to have a fixed code
-# 2. I only use the first 16 char in this code
-#    because hex only contains 0-9a-f
-# 3. The problem of this method is that if our 
+# 2. The problem of this method is that if our 
 #    server restarts, Link A will get a new shorten
 #    URL because CODE is chaged.
 CODE = list(string.ascii_letters + string.digits + 'z')
